ba2acfcbb87aade4d47ed07f2af566fc7c8eca92
Fauzan Deni Rywannis <fauzandeni176@gmail.com>
2020-07-31 07:25:47 +0700

    Correcting font size and adding new TextStyle

--- lib/style.dart
+++ lib/style.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const LargeTextSize = 24.0;
-const MediumTextSize = 18.0;
+const MediumTextSize = 20.0;
 const SmallTextSize = 16.0;
+
+const Color blue900 = Color(0xFF0D47A1);
 
 const String FontNameDefault = 'Montserrat';
 
@@ -21,8 +24,42 @@ const HeadTextStyle = TextStyle(
 );
 
 const BodyTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w400,
+  fontSize: SmallTextSize,
+  color: Colors.black
+);
+
+const CardTitleTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w500,
+  fontSize: MediumTextSize,
+  color: Colors.black
+);
+
+const CardBodyTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w500,
+  fontSize: SmallTextSize,
+  color: Colors.black
+);
+
+const FlatButtonTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w600,
+  fontSize: SmallTextSize,
+  color: blue900
+);
+
+const TabTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w600,
+  fontSize: SmallTextSize
+);
+
+const HintTextStyle = TextStyle(
     fontFamily: FontNameDefault,
     fontWeight: FontWeight.w400,
     fontSize: SmallTextSize,
-    color: Colors.black
+    color: Colors.grey
 );
